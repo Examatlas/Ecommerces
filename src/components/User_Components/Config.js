1ba806bf61c6efe0c1043f1a6ed43fe804b02cf4
examAtlas <crownclassesrnc@gmail.com>
2024-10-25 17:55:43 +0530

    ecommerce

--- src/components/User_Components/Config.js
+++ src/components/User_Components/Config.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = 'http://localhost:5000/api';
+// const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://ecommercesbackend.vercel.app/';
 // const API_BASE_URL = 'https://examatlas-backend.onrender.com/api';
 
 export default API_BASE_URL;
