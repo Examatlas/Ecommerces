7976b25187aa008596acecf03e6ce6aee1a603b4
examAtlas <crownclassesrnc@gmail.com>
2024-11-29 11:49:01 +0530

    Razorpay

--- src/components/User_Components/Book/BillingForm.jsx
+++ src/components/User_Components/Book/BillingForm.jsx
@@ -131,8 +131,8 @@ const BillingForm = () => {
 
     const token = localStorage.getItem('user_token');
 
- const REACT_APP_RAZORPAY_API_KEY= "rzp_test_Py5aXtaPQ5j9nu"
-//  const REACT_APP_RAZORPAY_API_KEY= "rzp_live_B3tlB0LBuRXlgF"
+//  const REACT_APP_RAZORPAY_API_KEY= "rzp_test_Py5aXtaPQ5j9nu"
+ const REACT_APP_RAZORPAY_API_KEY= "rzp_live_B3tlB0LBuRXlgF"
 
     const options = {
       key : REACT_APP_RAZORPAY_API_KEY ,
