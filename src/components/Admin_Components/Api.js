49edf16b2918ac8eda6b48833a10beabc22a008f
examAtlas <crownclassesrnc@gmail.com>
2024-12-05 18:23:27 +0530

    changes main url

--- src/components/Admin_Components/Api.js
+++ src/components/Admin_Components/Api.js
@@ -5,7 +5,8 @@ const token = localStorage.getItem("Admin_token");
 console.log(token,"token")
 // Create an Axios instance with base configuration
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_BASE_URL || 'http://localhost:5000/api', // Replace with your actual base URL
+  // baseURL: import.meta.env.VITE_BACKEND_BASE_URL || 'http://localhost:5000/api', // Replace with your actual base URL
+  baseURL: 'https://ecommerce-backend.crownpublications.in/api', // Replace with your actual base URL
   headers: {
     "Content-Type": "application/json",
     Authorization: token ? `Bearer ${token}` : "", // Attach token if available
--- src/components/User_Components/Config.js
+++ src/components/User_Components/Config.js
@@ -1,5 +1,5 @@
 // const API_BASE_URL = import.meta.env.VITE_BACKEND_BASE_URL || 'http://localhost:5000/api';
-const API_BASE_URL = 'https://ecommerce-backend-test.crownpublications.in/api';
+const API_BASE_URL = 'https://ecommerce-backend.crownpublications.in/api';
 
 export default API_BASE_URL;
 
